84166e1e5c51ce6db1f268227037e71f50a76093
Aolseth <anders.olseth@me.com>
2025-11-24 10:23:41 +0100

    update login database app.js

--- GruppeProsjekt/app.js
+++ GruppeProsjekt/app.js
@@ -18,8 +18,24 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+// database
+const { createDatabaseConnection } = require("./database");
+const sqlConfig = require("./sqlconfig");
+// database connection
+let db;
+(async () => {
+  try {
+    db = await createDatabaseConnection(sqlConfig);
+    app.locals.db = db;  // gjør databasen tilgjengelig i alle routes
+    console.log("Database connected");
+  } catch (err) {
+    console.error("Database connection failed:", err);
+  }
+})();
+
 // statiske filer
 app.use(express.static(path.join(__dirname, 'public')));
+
 
 // routes
 app.use('/', indexRouter);
@@ -28,5 +44,9 @@ app.use('/users', usersRouter);
 // SMS route (nå fungerer req.body)
 var smsRouter = require('./routes/sms');
 app.use('/api', smsRouter);
+
+//AUTH ROUTE
+const authRouter = require('./routes/auth');
+app.use('/auth', authRouter);
 
 module.exports = app;
