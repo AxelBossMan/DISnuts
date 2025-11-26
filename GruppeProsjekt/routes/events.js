e65ae91b3b3dc2730d86f6094c105506bb7207dc
AxelBossMan <axwoxen@gmail.com>
2025-11-26 13:34:22 +0100

    Update events.js

--- GruppeProsjekt/routes/events.js
+++ GruppeProsjekt/routes/events.js
@@ -3,35 +3,36 @@ const router = express.Router();
 const db = require("../database/sql");
 
 router.get("/", async (req, res) => {
-  if (!req.cookies.companySession) {
-    return res.redirect("/login.html");  
-  }
   try {
-    // 1. Hent ALLE events
-    const events = await db.readAll("event");
-
-    // 2. Finn company_id (antar alle events har samme company_id)
-    let companyName = null;
-    let companySlug = null;
-
-    if (events.length > 0) {
-      const companyId = events[0].company_id; 
-      const company = await db.getCompanyById(companyId);
-      companyName = company ? company.company_name : null;
-
-      if (companyName) {
-        companySlug = companyName
-          .trim()
-          .toLowerCase()
-          .replace(/\s+/g, "-");
-      }
+    if (!req.cookies.companySession) {
+      return res.redirect("/login.html");
     }
 
-    // 3. Send begge deler til frontend
+    const email = req.cookies.companySession;
+    const companyId = await db.getIdFromMail(email);
+
+    const events = await db.raw(`
+      SELECT * FROM dbo.event WHERE company_id = ${companyId}
+    `);
+
+    console.log("company id:", companyId, "email: ", email)
+    const company = await db.getCompanyById(companyId);
+
+    let companyName = company ? company.company_name : null;
+    let companySlug = null;
+
+    if (companyName) {
+      companySlug = companyName
+        .trim()
+        .toLowerCase()
+        .replace(/\s+/g, "-");
+    }
+
+    // 5) Send data til frontend
     res.json({
       events,
       company_name: companyName,
-      company_slug: companySlug 
+      company_slug: companySlug
     });
 
   } catch (err) {
@@ -39,5 +40,6 @@ router.get("/", async (req, res) => {
     res.status(500).json({ error: "Database error" });
   }
 });
+
 
 module.exports = router;
